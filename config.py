fd232a7f4a20832fb039831b047b27ce35893b97
Kurbanov Davyd <101460493+Maxwell012@users.noreply.github.com>
2023-06-01 22:19:02 +0200

    Create config.py
    add config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# Proxies
+API_KEY_PROXY = "your_api"    # I use proxies from https://proxy2.webshare.io/ which are the cheapest
+
+# Instantly
+API_KEY_INSTANTLY = "your_api"          # https://app.instantly.ai/app/settings/integrations
+CAMPAIGN_ID = "your_campaign"
+
+# Lamadava
+TOKEN = "your_token"    # if you want to use Lamadava then you need their token
+
+# Spreadsheet id
+SPREADSHEET_ID = ""   # if you want to use a spreadsheet then write a table id here
